f49b38a4fb4400ad6492109290627fce548f0a91
Nikita Zerniukov <62993541+daleunixal@users.noreply.github.com>
2021-11-08 21:00:12 +0500

    Update index.ts

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -1,6 +1,8 @@
 /** Задача 3 - Да кто эта ваща фабрика
  *
- * Реализуйте фабрику для сотрудников.
+ * Реализуйте фабрику сотрудников.
+ * По типу отдела работника, фабрика отдает экземпляр класса данного отдела
+ * Например: EmployeeFabric.createInstance(division.IT, 'Игорь') вернет IT'ника Игоря.
  * Материалы:
  * https://refactoring.guru/ru/design-patterns/factory-method
  * https://refactoring.guru/ru/design-patterns/abstract-factory
