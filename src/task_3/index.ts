c253408526476562008ff96adb01ff2322bc74a0
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-11-13 19:32:50 +0500

    Task 3 is solved

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -1,4 +1,4 @@
-/** Задача 3 - Да кто эта ваща фабрика
+/** Задача 3 - Да кто эта ваша фабрика
  *
  * Реализуйте фабрику сотрудников.
  * По типу отдела работника, фабрика отдает экземпляр класса данного отдела
@@ -6,5 +6,32 @@
  * Материалы:
  * https://refactoring.guru/ru/design-patterns/factory-method
  * https://refactoring.guru/ru/design-patterns/abstract-factory
-*/
+ */
+import {BaseEmployee, IManageEmployee} from "../task_1";
+import {EmployeeDivision} from "../empoyee-separate.enum";
+import {Accountant, Admin, ITSpecialist, Manager} from "../task_2";
 
+
+abstract class AbstractEmployeeFactory {
+    static createBaseEmployee = (department: EmployeeDivision.IT
+        | EmployeeDivision.calculus, name: string): BaseEmployee => {
+        return undefined;
+    };
+
+    static createManageEmployee = (department: EmployeeDivision, name: string):
+        IManageEmployee => {
+        return undefined;
+    };
+}
+
+export class EmployeeFactory extends AbstractEmployeeFactory {
+    public static createBaseEmployee(department: EmployeeDivision.IT
+        | EmployeeDivision.calculus, name: string): BaseEmployee {
+        return department === EmployeeDivision.IT ? new ITSpecialist(name) : new Accountant(name);
+    }
+
+    public static createManageEmployee(department: EmployeeDivision.management
+        | EmployeeDivision.administration, name: string): IManageEmployee {
+        return department === EmployeeDivision.management ? new Manager(name) : new Admin(name);
+    }
+}
