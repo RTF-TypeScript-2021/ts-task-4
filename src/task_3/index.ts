b2a7425b739e98ee8f66d609e3909f72d9c2d456
Darctys <yltimatym2002@maiil.ru>
2021-11-14 13:00:06 +0500

    Сычев Тимофей

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -7,4 +7,31 @@
  * https://refactoring.guru/ru/design-patterns/factory-method
  * https://refactoring.guru/ru/design-patterns/abstract-factory
 */
+import {BaseEmployee, IManageEmployee} from "../task_1";
+import {EmployeeDivision} from "../empoyee-separate.enum";
+import {Administrator, Calculus, Itworker, Manager} from "../task_2";
+export interface IEmployeeFactory {
+    makeManageWorker: (departure: EmployeeDivision.management | EmployeeDivision.administration, fullName: string) => IManageEmployee;
+    makeBaseWorker: (departure: EmployeeDivision.IT | EmployeeDivision.calculus, fullName: string) => BaseEmployee;
+}
+
+export class WorkerFactory implements IEmployeeFactory {
+    public makeManageWorker(departure: EmployeeDivision.management | EmployeeDivision.administration, fullName: string): IManageEmployee {
+        if (departure === EmployeeDivision.management) {
+            return new Manager(fullName);
+        }
+
+        return new Administrator(fullName);
+    }
+
+    public makeBaseWorker(departure: EmployeeDivision.IT | EmployeeDivision.calculus, fullName: string): BaseEmployee {
+        if (departure === EmployeeDivision.calculus) {
+            return new Calculus(fullName);
+        }
+
+        return new Itworker(fullName);
+    }
+}
+
+
 
--- src/task_4/index.ts
+++ src/task_4/index.ts
@@ -7,4 +7,28 @@
  * По сути это задание - песочница.
  * Вы можете конспектировать свои действия, для более глубокого понимания происходящего.
  */
+import {WorkerFactory} from "../task_3";
+import {EmployeeDivision} from "../empoyee-separate.enum";
 
+const jediCouncil = (factory: WorkerFactory) => {
+    const magisterJedi = factory.makeManageWorker(EmployeeDivision.administration, "Yoda");
+    const jedi = [];
+    const padawans = [];
+    const younglings = [];
+    jedi[0] = factory.makeManageWorker(EmployeeDivision.management, "Obi-wan Kenobi");
+    jedi[1] = factory.makeManageWorker(EmployeeDivision.management, "Anakin Skywalker");
+    padawans[0] = factory.makeBaseWorker(EmployeeDivision.calculus, "Ahsoka Tano");
+    padawans[1] = factory.makeBaseWorker(EmployeeDivision.calculus, "Luke Skywalker");
+    younglings[0] = factory.makeBaseWorker(EmployeeDivision.IT, "Ashla");
+    younglings[1] = factory.makeBaseWorker(EmployeeDivision.IT, "Katooni");
+    magisterJedi.addSubordinate(jedi[0]);
+    magisterJedi.addSubordinate(jedi[1]);
+    jedi[0].addSubordinate(padawans[0]);
+    jedi[1].addSubordinate(padawans[1]);
+    jedi[0].addSubordinate(younglings[0]);
+    jedi[1].addSubordinate(younglings[1]);
+    magisterJedi.getSubordinates();
+    jedi[1].getSubordinates();
+};
+
+jediCouncil(new WorkerFactory());
