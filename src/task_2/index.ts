02f746d5f45a90bc3dce16eb84f09b8af23a3c40
User <syperprosyper@gmail.com>
2021-12-30 21:27:22 +0500

    Воробьева Кристина

--- src/task_2/index.ts
+++ src/task_2/index.ts
@@ -7,3 +7,82 @@ import { EmployeeDivision } from "../empoyee-separate.enum";
  * Реализуйте необходимые методы.
  */
 
+export class Itworker extends BaseEmployee{
+    constructor(fullName: string) {
+        super(fullName, EmployeeDivision.IT);
+    }
+
+    public override getAuthority() {
+        console.log("программирую");
+    }
+}
+
+export class Calculus extends BaseEmployee{
+    constructor(fullName: string) {
+        super(fullName, EmployeeDivision.calculus);
+    }
+
+    public override getAuthority() {
+        console.log("считаю зарплату для Itworker");
+    }
+}
+
+export class Manager extends BaseEmployee implements IManageEmployee{
+    constructor(fullName: string) {
+        super(fullName, EmployeeDivision.management);
+    }
+
+    public override getAuthority() {
+        console.log("управляю работниками");
+    }
+
+    public subordinates: Map<EmployeeDivision, BaseEmployee[]> = new Map();
+
+    addSubordinate(person: BaseEmployee): void {
+        this.subordinates.get(person.getDepartment()).push(person);
+    }
+
+    getSubordinates(flatOutput?: boolean): void {
+        if (flatOutput){
+            console.log(this.subordinates.values());
+        }else{
+            console.log(this.subordinates.entries());
+        }
+    }
+
+    removeSubordinate(person: BaseEmployee): void {
+        const indexOfEmployee = this.subordinates.get(person.getDepartment()).indexOf(person);
+        this.subordinates.get(person.getDepartment()).splice(indexOfEmployee,1);
+
+    }
+}
+
+export class Administrator extends BaseEmployee implements IManageEmployee {
+    constructor(fullName: string) {
+        super(fullName, EmployeeDivision.administration);
+    }
+
+    public override getAuthority() {
+        console.log("управляю управляющими");
+    }
+
+    public subordinates: Map<EmployeeDivision, BaseEmployee[]> = new Map();
+
+    addSubordinate(person: BaseEmployee): void {
+        this.subordinates.get(person.getDepartment()).push(person);
+    }
+
+    getSubordinates(flatOutput?: boolean): void {
+        if (flatOutput) {
+            console.log(this.subordinates.values());
+        } else {
+            console.log(this.subordinates.entries());
+        }
+    }
+
+    removeSubordinate(person: BaseEmployee): void {
+        const indexOfEmployee = this.subordinates.get(person.getDepartment()).indexOf(person);
+        this.subordinates.get(person.getDepartment()).splice(indexOfEmployee, 1);
+
+    }
+}
