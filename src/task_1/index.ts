decf411ba4d7351d017b6b101cd5d27a6d672b0e
6loodrain <89647718+6loodrain@users.noreply.github.com>
2022-05-09 21:35:43 +0500

    Update index.ts

--- src/task_1/index.ts
+++ src/task_1/index.ts
@@ -1,4 +1,5 @@
 import { EmployeeDivision } from "../empoyee-separate.enum";
+import { EmployeeFabric } from "../task_3";
 
 /**
  * Задание 1 - директор тоже работник
@@ -25,10 +26,31 @@ import { EmployeeDivision } from "../empoyee-separate.enum";
  *
  *
  */
-
-export abstract class BaseEmployee {
-}
-
-export interface IManageEmployee {
-
-}
+ 
+ export abstract class BaseEmployee{
+     private _name: string;
+     private _division: EmployeeDivision;
+ 
+     public get division(): EmployeeDivision {
+         return this._division;
+     }
+     public get name(): string {
+         return this._name;
+     }
+ 
+     constructor(name: string, division: EmployeeDivision) {
+         this._name = name;
+         this._division = division;
+     }
+ 
+     public getAuthority() {
+         console.log(EmployeeFabric.divisionAuthoritys[this._division]);
+     }
+ }
+ 
+ export interface IManageEmployee {
+     subordinates: Map<EmployeeDivision, BaseEmployee[]>,
+     getSubordinates(flatOutput?: boolean): Map<EmployeeDivision, BaseEmployee[]> | BaseEmployee[],
+     addSubordinate(person: BaseEmployee): void,
+     removeSubordinate(person: BaseEmployee): void,
+ }
