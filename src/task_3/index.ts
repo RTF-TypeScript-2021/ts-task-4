662d874a40c95e22a33dd7868051511d01238a5e
6loodrain <89647718+6loodrain@users.noreply.github.com>
2022-05-09 21:36:16 +0500

    Update index.ts

--- src/task_3/index.ts
+++ src/task_3/index.ts
@@ -8,3 +8,28 @@
  * https://refactoring.guru/ru/design-patterns/abstract-factory
 */
 
+import { EmployeeDivision } from "../empoyee-separate.enum";
+import { BaseEmployee } from "../task_1";
+import { ManagerEmployee, ITEmployee, ManagementEmployee, AdministrationEmployee, CalculusEmployee} from "../task_2";
+
+export abstract class EmployeeFabric {
+    public static readonly divisionAuthoritys: Record<EmployeeDivision, string> = {
+        [EmployeeDivision.IT]: 'Works in the IT department',
+        [EmployeeDivision.calculus]: 'Works in the Calculus department',
+        [EmployeeDivision.management]: 'Works in the Management department, has subordinates',
+        [EmployeeDivision.administration]: 'Works as Administrator, has subordinates'
+    }
+
+    public static createInstance(name: string, divison: EmployeeDivision): BaseEmployee | ManagerEmployee {
+        switch (divison) {
+            case (EmployeeDivision.IT):
+                return new ITEmployee(name, divison);
+            case (EmployeeDivision.calculus):
+                return new CalculusEmployee(name, divison);
+            case (EmployeeDivision.management):
+                return new ManagementEmployee(name, divison, new Map<EmployeeDivision, BaseEmployee[]>());
+            case (EmployeeDivision.administration):
+                return new AdministrationEmployee(name, divison, new Map<EmployeeDivision, BaseEmployee[]>());
+        }
+    }
+}
